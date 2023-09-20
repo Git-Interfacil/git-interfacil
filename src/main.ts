9e6594a2325e25e529eb9f13409bd152573a64f2
José Lucas Silva Mayer <josemayer@usp.br>
2023-09-20 16:33:53 -0300

    Change indent size to 2 spaces
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,36 @@ const git_module = require("./git.ts");
 const { app, BrowserWindow } = electron;
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, "preload.ts"),
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.ts"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    win.loadFile("src/index.html");
+  win.loadFile("src/index.html");
 };
 
 app.on("ready", () => {
-    createWindow();
-    console.log(git_module.get_commit_info());
+  createWindow();
+  console.log(git_module.get_commit_info());
 
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
 
 const env = process.env.NODE_ENV || "development";
 if (env === "development") {
-    try {
-        require("electron-reloader")(module);
-        // eslint-disable-next-line no-empty
-    } catch (_) {}
+  try {
+    require("electron-reloader")(module);
+    // eslint-disable-next-line no-empty
+  } catch (_) {}
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -6,31 +6,29 @@ const commits = mocks.COMMITS_MOCK;
 const branches = Array.from(new Set(commits.map(({ branchId }) => branchId)));
 
 const branchesElements = branches.map((branch, i) => {
-    const branchElement = document.createElement("div");
-    branchElement.classList.add("branch");
-    branchElement.style.borderColor = constants.COLORS[i];
-    return { id: branch, branchElement, color: constants.COLORS[i] };
+  const branchElement = document.createElement("div");
+  branchElement.classList.add("branch");
+  branchElement.style.borderColor = constants.COLORS[i];
+  return { id: branch, branchElement, color: constants.COLORS[i] };
 });
 
 commits.sort(
-    (a, b) => new Date(b.createdAt).valueOf() - new Date(a.createdAt).valueOf(),
+  (a, b) => new Date(b.createdAt).valueOf() - new Date(a.createdAt).valueOf(),
 );
 
 commits.forEach(({ branchId }, i) => {
-    const parentBranchElement = branchesElements.find(
-        ({ id }) => branchId === id,
-    );
-    if (!parentBranchElement) return;
-    const commitElement = document.createElement("div");
-    commitElement.classList.add("commit");
-    commitElement.style.top = `${-10 + i * 40}px`;
-    commitElement.style.borderColor = parentBranchElement.color;
-    parentBranchElement.branchElement.appendChild(commitElement);
+  const parentBranchElement = branchesElements.find(
+    ({ id }) => branchId === id,
+  );
+  if (!parentBranchElement) return;
+  const commitElement = document.createElement("div");
+  commitElement.classList.add("commit");
+  commitElement.style.top = `${-10 + i * 40}px`;
+  commitElement.style.borderColor = parentBranchElement.color;
+  parentBranchElement.branchElement.appendChild(commitElement);
 });
 
 branchesElements.forEach(
-    ({ branchElement }) =>
-        document
-            .getElementById("branches-container")
-            ?.appendChild(branchElement),
+  ({ branchElement }) =>
+    document.getElementById("branches-container")?.appendChild(branchElement),
 );
