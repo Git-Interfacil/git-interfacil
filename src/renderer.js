9704414fe925720f9058de889168026e00320b32
José Lucas Silva Mayer <josemayer@usp.br>
2023-09-28 01:18:31 -0300

    Rewrite render with dinamically canvas size
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/renderer.js
+++ src/renderer.js
@@ -2,40 +2,61 @@ const constants = require("./constants.js");
 const mocks = require("./mocks.js");
 const canvasController = require("./canvasController.js");
 
-const commits = mocks.COMMITS_MOCK.commits;
-const head = mocks.COMMITS_MOCK.head;
+function setCanvasSize(canvas, num_branches, num_commits) {
+  canvas.width = constants.COLUMN_WIDTH * (num_branches + 1);
+  canvas.height = constants.LINE_HEIGHT * (num_commits + 1);
+}
 
-const canvas = document.querySelector("canvas");
-if (!canvas) throw Error("No canvas found");
-const ctx = canvas.getContext("2d");
-if (!ctx) throw Error("No 2d context found");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function generateBranches(commits) {
+  const branches = [];
+  commits.forEach(({ branchId }, ind) => {
+    let branch = branches.find(({ id }) => id === branchId);
+    if (!branch) {
+      branch = {
+        id: branchId,
+        pos: {
+          x: (branches.length + 1) * constants.COLUMN_WIDTH,
+          y: (ind + 1) * constants.LINE_HEIGHT,
+        },
+        color: constants.COLORS[branches.length],
+      };
+      branches.push(branch);
+    }
+  });
+  return branches;
+}
 
-commits.sort(
-  (a, b) => new Date(b.createdAt).valueOf() - new Date(a.createdAt).valueOf(),
-);
+function drawBranches(ctx, head, commits, branches) {
+  commits.forEach(({ branchId, id: commitId }, ind) => {
+    const parentBranch = branches.find(({ id }) => id === branchId);
 
-const branches = [];
-commits.forEach(({ branchId, id: commitId }, ind) => {
-  let parentBranch = branches.find(({ id }) => id === branchId);
-  if (!parentBranch) {
-    parentBranch = {
-      id: branchId,
-      pos: {
-        x: (branches.length + 1) * constants.COLUMN_WIDTH,
-        y: (ind + 1) * constants.LINE_HEIGHT,
-      },
-      color: constants.COLORS[branches.length],
-    };
-    branches.push(parentBranch);
-  }
+    const pos = { x: parentBranch.pos.x, y: (ind + 1) * constants.LINE_HEIGHT };
 
-  const pos = { x: parentBranch.pos.x, y: (ind + 1) * constants.LINE_HEIGHT };
+    if (head == commitId)
+      canvasController.drawCommit(ctx, pos, parentBranch.color, 8, true);
+    else canvasController.drawCommit(ctx, pos, parentBranch.color);
 
-  if (head == commitId)
-    canvasController.drawCommit(ctx, pos, parentBranch.color, 8, true);
-  else canvasController.drawCommit(ctx, pos, parentBranch.color);
+    canvasController.drawLine(ctx, pos, parentBranch.pos, parentBranch.color);
+  });
+}
 
-  canvasController.drawLine(ctx, pos, parentBranch.pos, parentBranch.color);
-});
+function main() {
+  const commits = mocks.COMMITS_MOCK.commits;
+
+  commits.sort(
+    (a, b) => new Date(b.createdAt).valueOf() - new Date(a.createdAt).valueOf(),
+  );
+
+  const branches = generateBranches(commits);
+  const head = mocks.COMMITS_MOCK.head;
+
+  const canvas = document.querySelector("canvas");
+  if (!canvas) throw Error("No canvas found");
+  const ctx = canvas.getContext("2d");
+  if (!ctx) throw Error("No 2d context found");
+
+  setCanvasSize(canvas, branches.length, commits.length);
+  drawBranches(ctx, head, commits, branches);
+}
+
+main();
