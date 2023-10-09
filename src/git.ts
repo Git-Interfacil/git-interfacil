c9dfdb9446925e018bfac3fde4817b63bc3aeea8
luamfb <mcmfbarbosa@gmail.com>
2023-10-09 14:48:47 -0300

    Modify API to receive repository path

--- src/git.ts
+++ src/git.ts
@@ -1,13 +1,21 @@
 const execSync = require("child_process").execSync;
 const formatStr = '{"hash":"%h","author":"%an","message":"%s"}';
 
-module.exports = {
+class Repository {
+  constructor(path) {
+    this.path = path;
+  }
   // TODO
   // - change shell's current directory to where the user's project is
   // - get branch from commit hash
-  get_commit_info: function () {
+  get_commit_info() {
     return execSync(`git log --format='format:${formatStr}'`, {
+      cwd: this.path,
       encoding: "utf8",
     });
-  },
+  }
+}
+
+module.exports = {
+  Repository,
 };
