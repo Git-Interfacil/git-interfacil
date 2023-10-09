e15d0a3d6055daf4f2e83fc8ef9c8643e2959ece
luamfb <mcmfbarbosa@gmail.com>
2023-10-09 15:09:00 -0300

    Allow updating repository path

--- src/git.ts
+++ src/git.ts
@@ -2,9 +2,15 @@ const execSync = require("child_process").execSync;
 const formatStr = '{"hash":"%h","author":"%an","message":"%s"}';
 
 class Repository {
-  constructor(path) {
+  update_repo_path(path) {
     this.path = path;
+    // TODO get HEAD
   }
+
+  constructor(path) {
+    this.update_repo_path(path);
+  }
+
   // TODO
   // - change shell's current directory to where the user's project is
   // - get branch from commit hash
