ef3c04b13cad53b2a4629cb7c900113b6b1f44da
fernanda itoda <fitoda@usp.br>
2023-10-16 19:25:43 -0300

    adding buttons

---
+++ src/buttonHandler.js
@@ -0,0 +1,48 @@
+const undoButton = document.getElementById("undoButton");
+const redoButton = document.getElementById("redoButton");
+const mergeButton = document.getElementById("mergeButton");
+const pullButton = document.getElementById("pullButton");
+const pushButton = document.getElementById("pushButton");
+const branchButton = document.getElementById("branchButton");
+const stashButton = document.getElementById("stashButton");
+const popButton = document.getElementById("popButton");
+
+undoButton.addEventListener("click", function () {
+  console.log("Undo button clicked");
+  // #TODO: Add your code here for the Undo button action
+});
+
+redoButton.addEventListener("click", function () {
+  console.log("Redo button clicked");
+  // #TODO: Add your code here for the Redo button action
+});
+
+mergeButton.addEventListener("click", function () {
+  console.log("Merge button clicked");
+  // #TODO: Add your code here for the Merge button action
+});
+
+pullButton.addEventListener("click", function () {
+  console.log("Pull button clicked");
+  // #TODO: Add your code here for the Pull button action
+});
+
+pushButton.addEventListener("click", function () {
+  console.log("Push button clicked");
+  // #TODO: Add your code here for the Push button action
+});
+
+branchButton.addEventListener("click", function () {
+  console.log("Branch button clicked");
+  // #TODO: Add your code here for the Branch button action
+});
+
+stashButton.addEventListener("click", function () {
+  console.log("Stash button clicked");
+  // #TODO: Add your code here for the Stash button action
+});
+
+popButton.addEventListener("click", function () {
+  console.log("Pop button clicked");
+  // #TODO: Add your code here for the Pop button action
+});
