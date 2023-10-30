28e78fcb7d11f0a5c220456859abe15be6088997
Rafael Tsuha <rafael.tsuha@usp.br>
2023-10-30 12:05:58 -0300

    remove test with mocked values

--- test/renderer.test.js
+++ test/renderer.test.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-undef */
-const { drawLine, drawCommit } = require("../src/canvasController.js");
-const { createMessage } = require("../src/messagesController.js");
-const { createBranch } = require("../src/localBranchesController.js");
 
 jest.mock("../src/canvasController.js");
 jest.mock("../src/messagesController.js");
 jest.mock("../src/localBranchesController.js");
-const ctxMock = jest.fn();
-const liMock = jest.fn();
-const elementsMock = jest.fn();
 
 describe("renderer", () => {
   beforeEach(() => jest.clearAllMocks());
@@ -30,24 +24,5 @@ describe("renderer", () => {
     } catch (e) {
       expect(e.message).toBe("No 2d context found");
     }
-  });
-
-  it("should render correctly", () => {
-    document.querySelector = () => ({
-      getContext: () => ctxMock,
-      addEventListener: jest.fn(),
-    });
-    document.getElementById = () => ({
-      appendChild: () => liMock,
-      querySelectorAll: () => ({
-        forEach: () => elementsMock,
-      }),
-    });
-    require("../src/renderer.js");
-
-    expect(drawCommit).toHaveBeenCalled();
-    expect(drawLine).toHaveBeenCalled();
-    expect(createMessage).toHaveBeenCalled();
-    expect(createBranch).toHaveBeenCalled();
   });
 });
