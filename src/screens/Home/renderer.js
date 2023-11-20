171ae61596c0e492c4819ae93241d082b068618c
fernanda <fitoda@usp.br>
2023-11-20 16:31:32 -0300

    adding other pages to home

--- src/screens/Home/renderer.js
+++ src/screens/Home/renderer.js
@@ -1,24 +1,26 @@
 const ipcRendererManager = require("../../utils/ipcRendererManager");
 
-function main() {
-  document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll("#sidebar button");
+// Function to fetch content from newContent.html
+function fetchNewContent(pageName) {
+  fetch(`${pageName}.html`) // Fetch the content from the HTML file
+    .then((response) => response.text()) // Convert response to text
+    .then((html) => {
+      // Replace content inside the main container with the fetched HTML
+      const mainContainer = document.getElementById("main");
+      mainContainer.innerHTML = html;
 
-    // TO-DO: add changing screen function
-    buttons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        if (button.classList.contains("selected")) {
-          button.classList.remove("selected");
-        } else {
-          buttons.forEach(function (btn) {
-            btn.classList.remove("selected");
-            button.classList.add("selected");
-          });
-        }
-      });
+      if (pageName === "workspaces") {
+        workspaces();
+      } else if (pageName === "help") {
+        help();
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching content:", error);
     });
-  });
+}
 
+function workspaces() {
   const button = document.getElementById("newWorkspaceButton");
 
   button.addEventListener("click", function () {
@@ -27,6 +29,41 @@ function main() {
 
   ipcRendererManager.listenToMain("selected-folder", (event, path) => {
     ipcRendererManager.showScreenWithData("index", { path: path });
+  });
+}
+
+// TO-DO: write the questions ans answers in html
+function help() {
+  const questions = document.querySelectorAll(".question");
+
+  questions.forEach((question) => {
+    question.addEventListener("click", () => {
+      const answer = question.nextElementSibling;
+      answer.style.display =
+        answer.style.display === "block" ? "none" : "block";
+    });
+  });
+}
+
+function main() {
+  fetchNewContent("workspaces");
+  document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll("#sidebar button");
+
+    // TO-DO: add changing screen function
+    buttons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        buttons.forEach(function (btn) {
+          btn.classList.remove("selected");
+          button.classList.add("selected");
+
+          const titleButton = button
+            .querySelector(".title")
+            .textContent.toLowerCase();
+          fetchNewContent(titleButton);
+        });
+      });
+    });
   });
 }
 
